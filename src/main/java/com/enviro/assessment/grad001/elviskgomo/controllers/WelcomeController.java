6326fd0aa6e6d03422a37837276ee76f618a551b
esnoko <elvisnoko18@gmail.com>
2025-01-18 22:41:56 +0200

    Updated

---
+++ src/main/java/com/enviro/assessment/grad001/elviskgomo/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.elviskgomo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to the Waste Management API";
+    }
+} 
